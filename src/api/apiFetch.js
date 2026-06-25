bb2a71533f440124fbea1f7bedfecba59277c5c9
Kateleen Issa <kateleenissa@Jimis-MacBook-Air-2.local>
2026-06-24 20:41:17 -0400

    Fix premature redirect to login on reload, remove unused CSRF code

--- src/api/apiFetch.js
+++ src/api/apiFetch.js
@@ -1,9 +1,7 @@
 import { API_BASE_URL } from "./config";
-import { getCsrfToken } from "./csrf";
 
 const UNSAFE_METHODS = ["POST", "PUT", "PATCH", "DELETE"];
 
-// options = body or params
 export const apiFetch = (path, options = {}) => {
   const method = (options.method || "GET").toUpperCase();
   return fetch(`${API_BASE_URL}${path}`, {
@@ -11,9 +9,6 @@ export const apiFetch = (path, options = {}) => {
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
-      ...(UNSAFE_METHODS.includes(method)
-        ? { "X-CSRFToken": getCsrfToken() }
-        : {}),
       ...options.headers,
     },
   });
--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,8 +2,14 @@ import { useAuth } from "../../context/AuthContext";
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function ProtectedRoute () {
-    const { user } = useAuth();
+    const { user, isLoading } = useAuth();
 
+    //check first is user is logged in - if so, do nothing
+    if (isLoading) {
+        return null;
+    }
+
+    //if user is not logged in, then redirect to login page
     if (!user) {
         return <Navigate to="/login" replace />;
     }
